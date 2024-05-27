0d8b99a6cdcdb372d4820da80a4ffe7ef59fc477
Firdavs-Normurodov <firdavsnormurodov435@gmail.com>
2024-05-27 13:08:26 +0500

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,35 @@
+const path = require("path"); // Import path module for resolving file paths
+
 module.exports = {
   mode: "development",
   entry: "./src/js/script.js",
   output: {
-    path: __dirname + "/dist/js",
+    path: path.resolve(__dirname, "dist/js"), // Use path.resolve to generate absolute paths
     filename: "bundle.js",
   },
   watch: true,
   devtool: "source-map", // Corrected typo here
-  module: {},
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|browser_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              [
+                "@babel/preset-env", // Corrected typo here
+                {
+                  debug: true,
+                  corejs: 3,
+                  useBuiltIns: "usage",
+                },
+              ],
+            ],
+          },
+        },
+      },
+    ],
+  },
 };
